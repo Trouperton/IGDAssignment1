5f207fc156dfc0d5d91634e55244fdd140f8efc6
Trouperton <58820346+Trouperton@users.noreply.github.com>
2025-04-23 15:17:02 +0100

    Initial commit

---
+++ Source/IGDAssignment1/IGDAssignment1GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "IGDAssignment1GameModeBase.h"
+
---
+++ Source/IGDAssignment1/IGDAssignment1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IGDAssignment1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IGDASSIGNMENT1_API AIGDAssignment1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
